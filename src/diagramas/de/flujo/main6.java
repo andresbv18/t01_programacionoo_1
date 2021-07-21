bd32b217b952f3076654a9d425f85e11306e1464
USER <USER@DESKTOP-L81LA4S>
2021-07-21 18:49:25 -0500

    TALLER:5

---
+++ src/diagramas/de/flujo/main6.java
@@ -0,0 +1,10 @@
+package diagramas.de.flujo;
+
+public class main6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/tipos/de/datos/MainTiposDeDatos.java
@@ -0,0 +1,23 @@
+package tipos.de.datos;
+
+public class MainTiposDeDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short CantidaDeEstudiantesUCE=1000;
+		byte EntradaCircuito=0;
+		int CantidadDeCiudadanos=10000;
+		long CantiadadMundo=1000000000;
+		float Sueldo = 1000.56f;
+		double SaldoBanco=100000.59;
+		char OpcionMenu;
+		int Año=3;
+		boolean Casado=true;
+		
+		
+		
+		
+		
+	}
+
+}
