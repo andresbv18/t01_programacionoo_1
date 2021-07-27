5fcd05843e311e7dcc7589a249abf75b6ede25d5
USER <USER@DESKTOP-L81LA4S>
2021-07-26 20:01:20 -0500

    Prueba2

---
+++ src/marco/andre/pozo/briones/Main.java
@@ -0,0 +1,28 @@
+package marco.andre.pozo.briones;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner lector= new Scanner(System.in);
+		int numero;
+		 System.out.print("Ingresen un numero");
+		numero=lector.nextInt();
+
+
+	while(numero!=-1)
+	{
+	System.out.print("Ingresen un numero correcto");
+	numero=lector.nextInt();
+	if(numero==-1) {
+		int suma=numero;
+	}
+		 System.out.println("*");
+
+
+	}
+
+}
+}
